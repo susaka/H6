2029d38ab008c0984713c6b4fbbb52beeb76b250
susaka <susaka114@gmail.com>
2021-05-31 12:28:47 +0200

    DHCP rolve v0.14

--- roles/DHCP/scripts/createPSscript.py
+++ roles/DHCP/scripts/createPSscript.py
@@ -12,13 +12,13 @@ ENV.filters.update(f.filters())
 #Load the 2 used group vars YAML files
 with open("./group_vars/VibDisSW/VibDisSW.yml") as inputfile:
     scope1 =  yaml.full_load(inputfile)
-#with open("./group_vars/RosDisSW/RosDisSW.yml") as inputfile:
-#    scope2 =  yaml.full_load(inputfile)
+with open("./group_vars/RosDisSW/RosDisSW.yml") as inputfile:
+    scope2 =  yaml.full_load(inputfile)
 
 #Get the template from the DHCP role template folder
 template = ENV.get_template("./roles/DHCP/templates/dhcp.j2")
 
 #write to file and save it at the role/script folder.
 with open("./roles/DHCP/scripts/final_dhcp.ps1", "w") as file:
-    file.write(template.render(scope1=scope1))
+    file.write(template.render(scope1=scope1,scope2=scope2))
 
