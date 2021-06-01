1fb1873862ea4f4c663988d856a90650caad3d27
susaka <susaka114@gmail.com>
2021-06-01 02:48:04 +0200

    radius role 0.3

--- roles/SetupRadius/files/createPSscript.py
+++ roles/SetupRadius/files/createPSscript.py
@@ -7,7 +7,10 @@ ENV = Environment(loader=FileSystemLoader('./'))
 #Get the template from the DHCP role template folder
 template = ENV.get_template("./roles/SetupRadius/templates/radius.j2")
 
+with open("./hosts.yml") as inputfile:
+    groups =  yaml.full_load(inputfile)
+
 #write to file and save it at the role/script folder.
 with open("./roles/SetupRadius/files/final_radius.ps1", "w") as file:
-    file.write(template.render())
+    file.write(template.render(groups=groups))
 
